0b32195742f9fcb1806d7e8f478100d0e12df24d
stz7750 <rlaxogns7750@gmail.com>
2024-07-22 17:48:26 +0900

    mybatis config Map type null 값 처리, MUI 추가 , log4Jdbc config SQL logging 추가

--- src/adminpages/MenuManageMent.js
+++ src/adminpages/MenuManageMent.js
@@ -11,93 +11,22 @@
  */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import MenuTable from './MenuTable';
 import MenuForm from './MenuForm';
-import { Treebeard } from 'react-treebeard';
 import trans from '../commons/trans';
 import { log, stzUtil } from '../commons/stzUtil';
+import MaterialTable from 'material-table';
+import { Switch, Grid, IconButton } from '@mui/material';
+import {
+  KeyboardArrowRight,
+  KeyboardArrowDown,
+  Delete,
+  Search,
+  Clear,
+} from '@mui/icons-material';
 
 const MenuManagement = () => {
   const [menus, setMenus] = useState([]);
   const [selectedMenu, setSelectedMenu] = useState(null);
-
-  const treeStyles = {
-    tree: {
-      base: {
-        listStyle: 'none',
-        backgroundColor: 'white',
-        margin: 0,
-        padding: 0,
-        color: '#000',
-        fontFamily: 'Arial, sans-serif',
-        fontSize: '14px',
-      },
-      node: {
-        base: {
-          position: 'relative',
-        },
-        link: {
-          cursor: 'pointer',
-          position: 'relative',
-          padding: '0px 5px',
-          display: 'block',
-        },
-        activeLink: {
-          background: '#ddd',
-        },
-        toggle: {
-          base: {
-            position: 'relative',
-            display: 'inline-block',
-            verticalAlign: 'middle',
-            marginLeft: '-5px',
-            height: '24px',
-            width: '24px',
-          },
-          wrapper: {
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            margin: '-7px 0 0 -7px',
-            height: '14px',
-          },
-          height: 14,
-          width: 14,
-          arrow: {
-            fill: '#9DA5AB',
-            strokeWidth: 0,
-          },
-        },
-        header: {
-          base: {
-            display: 'inline-block',
-            verticalAlign: 'middle',
-            color: '#000',
-          },
-          connector: {
-            width: '2px',
-            height: '12px',
-            borderLeft: 'solid 2px black',
-            borderBottom: 'solid 2px black',
-            position: 'absolute',
-            top: '0px',
-            left: '-21px',
-          },
-          title: {
-            lineHeight: '24px',
-            verticalAlign: 'middle',
-          },
-        },
-        subtree: {
-          listStyle: 'none',
-          paddingLeft: '19px',
-        },
-        loading: {
-          color: '#E2C089',
-        },
-      },
-    },
-  };
 
   useEffect(() => {
     fetchMenus();
@@ -105,9 +34,9 @@ const MenuManagement = () => {
 
   const fetchMenus = async () => {
     const response = await trans.get('/admin/api/getAllMenus');
-    const menuTree = stzUtil.buildTree(response.data, 'menuId', 'parentMenuId');
-    setMenus(menuTree);
-    log(menuTree);
+    /*const menuTree = stzUtil.buildTree(response.data, 'menuId', 'parentMenuId');*/
+    setMenus(response.data);
+    log(response.data);
   };
 
   const handleToggle = (node, toggled) => {
@@ -138,11 +67,95 @@ const MenuManagement = () => {
     setSelectedMenu(null);
   };
 
+  const handleDeleteMenu = async (menuId) => {
+    await axios.delete(`/admin/api/${menuId}`);
+    fetchMenus();
+  };
+
+  const handleToggleChange = async (menu) => {
+    const updatedMenu = {
+      menuId: menu.menuId,
+      useYn: menu.useYn === 'Y' ? 'N' : 'Y',
+    };
+
+    try {
+      await trans.post(`/admin/api/upsertMenuById`, updatedMenu);
+      // 상태를 직접 업데이트하여 테이블을 다시 렌더링
+      setMenus((prevMenus) =>
+        prevMenus.map((m) =>
+          m.menuId === menu.menuId ? { ...m, useYn: updatedMenu.useYn } : m,
+        ),
+      );
+    } catch (error) {
+      console.error('Error updating menu:', error);
+    }
+  };
+
+  const columns = [
+    {
+      title: '사용 여부',
+      field: 'useYn',
+      render: (rowData) => (
+        <Switch
+          checked={rowData.useYn === 'Y'}
+          onChange={() => handleToggleChange(rowData)}
+          color="primary"
+        />
+      ),
+    },
+    { title: 'Menu Name', field: 'menuName' },
+    { title: 'Menu ID', field: 'menuId' },
+    { title: 'Menu Path', field: 'menuPath' },
+  ];
+
   return (
-    <div>
+    <div style={{ padding: '20px' }}>
       <h1>메뉴관리</h1>
-      <Treebeard data={menus} onToggle={handleToggle} style={treeStyles} />
-      <MenuForm menu={selectedMenu} onSubmit={handleFormSubmit} menus={menus} />
+      <Grid container spacing={3}>
+        <Grid item xs={8}>
+          <MaterialTable
+            title="페이지 메뉴"
+            columns={columns}
+            data={menus}
+            parentChildData={(row, rows) =>
+              rows.find((a) => a.menuId === row.parentMenuId)
+            }
+            options={{
+              paging: true,
+              pageSize: 10,
+              pageSizeOptions: [10, 20, 50],
+              sorting: true,
+              maxBodyHeight: '60vh',
+              minBodyHeight: '60vh',
+            }}
+            icons={{
+              Search: () => <Search style={{ color: 'blue' }} />, // 검색 아이콘 변경
+              ResetSearch: () => <Clear style={{ color: 'red' }} />, // 검색 초기화 아이콘 변경
+              DetailPanel: ({ open }) =>
+                open ? <KeyboardArrowDown /> : <KeyboardArrowRight />,
+              Delete: () => <Delete style={{ color: 'red' }} />,
+            }}
+            editable={
+              {
+                /*onRowDelete: (oldData) =>
+                new Promise((resolve, reject) => {
+                  handleDeleteMenu(oldData.menuId);
+                  resolve();
+                }),*/
+              }
+            }
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <div style={{ height: '60vh', overflow: 'auto' }}>
+            <MenuForm
+              menu={selectedMenu}
+              onSubmit={handleFormSubmit}
+              menus={menus}
+            />
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 };
